4f079be8cb4d7f63f88c81da9e0224aa3c9a384d
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-21 18:18:10 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,28 @@
 class Show < ActiveRecord::Base
+  belongs_to :network
+  has_many :characters 
+  has_many :actors, through: :characters
   
+  def actors_list
+    #returns array off full names of actor associated with a show
+    self.characters.map do |actors|
+      "#{actors.first_name} #{actors.last_name}"
+    end
+  end
 end
+
+
+
+    
+    
+    
+    #show.characters
+    
+    #character.actor
+    
+    #actor.first_name
+    #actor.last_name
+    
+   #self.characters.map do |character|
+      #character.actor
+    #end
