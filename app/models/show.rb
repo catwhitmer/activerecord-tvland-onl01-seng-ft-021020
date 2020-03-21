6e2d27fff87dfce9e71b687b55a8c3e3a0332726
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-21 18:26:29 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+  belongs_to :network
+  has_many :characters 
+  has_many :actors, through: :characters
   
+  def actors_list
+    #returns array off full names of actor associated with a show
+    self.actors.collect do |t|
+      "#{t.first_name} #{t.last_name}"
+    end
+  end
 end
+
+
