82b14b87b1c2e0451c71a17cd66bae0e3f494278
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-21 18:22:57 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
   def actors_list
     #returns array off full names of actor associated with a show
     self.actors.collect |t|
-      "#{t.first_name} {t.last_name}"
+      "#{t.first_name} #{t.last_name}"
     end
   end
 end
